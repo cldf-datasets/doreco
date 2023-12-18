275bbd507ed5452c9184cf71b8ca92156978db21
Robert Forkel <xrotwang@googlemail.com>
2023-12-18 12:16:47 +0100

    v1.0.rc4

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
 
 def test_valid(cldf_dataset, cldf_logger):
+    clts_ids = [r['CLTS_ID'] for r in cldf_dataset['ParameterTable']]
+    assert len(set(clts_ids)) == len(clts_ids)
     assert cldf_dataset.validate(log=cldf_logger)
